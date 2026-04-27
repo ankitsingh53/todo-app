30a8d06b435731d2be0c79f2172008af5ea1a514
Ankit Kumar Singh <ankit.ks@celestialsys.com>
2026-04-27 23:48:08 +0530

    Add Time in Todo App

--- script.js
+++ script.js
@@ -1,59 +1,68 @@
-const inputBox = document.getElementById('inputBox')
-const addbtn = document.getElementById('addbtn')
-const todolist = document.getElementById('todolist')
+const inputBox = document.getElementById("inputBox");
+const addbtn = document.getElementById("addbtn");
+const todolist = document.getElementById("todolist");
 
 let editTodo = null;
 
+const now = new Date();
+console.log(now);
+
+const time = now.toLocaleTimeString();
+
+console.log(time);
 
 // --------Add to do----------
 
-const addTodo = ()=>{
-    console.log(inputBox.value)
-    
-    const inputText = inputBox.value.trim();
-    if(inputText.length <=0)
-        alert("You must write something in your to do")
-    else if (addbtn.value === "Edit"){
-        editTodo.target.previousElementSibling.innerHTML = inputText;
-        addbtn.value="Add";
-        inputBox.value = "";
-    }
-    else {
+const addTodo = () => {
+  console.log(inputBox.value);
+
+  const inputText = inputBox.value.trim();
+  if (inputText.length <= 0) alert("You must write something in your to do");
+  else if (addbtn.value === "Edit") {
+    editTodo.target.previousElementSibling.innerHTML = inputText;
+    addbtn.value = "Add";
+    inputBox.value = "";
+    console.log(editTodo.target)
+  } else {
     const li = document.createElement("li");
     const para = document.createElement("p");
     para.innerText = inputText;
-    li.appendChild(para)
+    li.appendChild(para);
 
+    const editBtn = document.createElement("button");
+    editBtn.innerText = "Edit";
+    li.appendChild(editBtn);
 
-    const editBtn = document.createElement("button")
-    editBtn.innerText = "Edit"
-    li.appendChild(editBtn)
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Remove";
+    li.appendChild(deleteBtn);
 
+    // adding time
 
-    const deleteBtn = document.createElement("button")
-    deleteBtn.innerText = "Remove"
-    li.appendChild(deleteBtn)
+    const span = document.createElement("span");
+    span.innerHTML = `  ${time}`;
+    li.appendChild(span);
 
     todolist.appendChild(li);
-    inputBox.value="";
-    }
-}
+    inputBox.value = "";
+  }
+};
 
 // ---Update Todo to edit and remove------------
 
-const updateTodo = (e)=>{
-   if(e.target.innerHTML === "Remove"){
-   todolist.removeChild(e.target.parentElement)
-   }
+const updateTodo = (e) => {
+  if (e.target.innerHTML === "Remove") {
+    todolist.removeChild(e.target.parentElement);
+  }
 
-   if(e.target.innerHTML === "Edit"){
+  if (e.target.innerHTML === "Edit") {
     inputBox.value = e.target.previousElementSibling.innerHTML;
 
-   inputBox.focus();
-   addbtn.value = "Edit";
-   editTodo = e;
-   }
-}
+    inputBox.focus();
+    addbtn.value = "Edit";
+    editTodo = e;
+  }
+};
 
-addbtn.addEventListener('click',addTodo);
-todolist.addEventListener('click', updateTodo)
+addbtn.addEventListener("click", addTodo);
+todolist.addEventListener("click", updateTodo);
